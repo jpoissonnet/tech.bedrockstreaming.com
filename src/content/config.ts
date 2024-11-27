391b6ba6b4fba41713acd667afcb08499f79103c
Jules Poissonnet <72744048+jpoissonnet@users.noreply.github.com>
2024-11-27 14:20:31 +0100

    refactor(astro): migrate the blog to astro (#1)

---
+++ src/content/config.ts
@@ -0,0 +1,67 @@
+import { defineCollection, reference, z } from "astro:content";
+
+const postsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      author: reference("authors").or(z.array(reference("authors"))),
+      title: z.string(),
+      subtitle: z.string().optional(),
+      description: z.string().optional(),
+      thumbnail: image().optional(),
+      tags: z.union([z.string(), z.number()]).array(),
+      layout: z.string(),
+    }),
+});
+
+const authorsCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image().optional().or(z.string().url()),
+      url: z.string().url().optional(),
+    }),
+});
+
+const lftsCollection = defineCollection({
+  type: "content",
+  schema: () =>
+    z.object({
+      layout: z.string(),
+      title: z.string(),
+      description: z.string().optional(),
+      tags: z.union([z.string(), z.number()]).array().optional(),
+      youtubeId: z.string().optional(),
+      eventName: z.string().optional(),
+      eventUrl: z.string().url().optional(),
+      sponsored: z.boolean().optional(),
+      hosted: z.boolean().optional(),
+      language: z.string().optional(),
+      lang: z.string().optional(),
+      author: reference("authors").or(z.array(reference("authors"))),
+    }),
+});
+
+const conferencesCollection = defineCollection({
+  type: "content",
+  schema: () =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      tags: z.union([z.string(), z.number()]).array().optional(),
+      youtubeId: z.string().optional(),
+      eventName: z.string(),
+      eventUrl: z.string().url(),
+      sponsored: z.boolean().optional(),
+      hosted: z.boolean().optional(),
+      author: reference("authors").or(z.array(reference("authors"))),
+    }),
+});
+
+export const collections = {
+  posts: postsCollection,
+  authors: authorsCollection,
+  lfts: lftsCollection,
+  conferences: conferencesCollection,
+};
